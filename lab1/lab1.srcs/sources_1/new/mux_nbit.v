a315dc2df804f07b4c327d0339c3d05cbec6c804
Teong Seng Tan <tanteongseng2@gmail.com>
2026-04-10 13:20:04 -0700

    Completed mux_nbit

--- lab1/lab1.srcs/sources_1/new/mux_nbit.v
+++ lab1/lab1.srcs/sources_1/new/mux_nbit.v
@@ -30,7 +30,7 @@ module mux_nbit #(N=4) (
     wire [N-1:0] select_b;
     wire [2**N-1:0] input_one_hot;
 
-    not (select_b, select);
+    not select_not [N-1:0] (select_b, select);
     
     genvar i, j;
     generate
@@ -41,7 +41,11 @@ module mux_nbit #(N=4) (
         end
     endgenerate
 
-    and (input_one_hot, inputs, select_one_hot);
+    and input_and [2**N-1:0] (input_one_hot, inputs, select_one_hot);
 
-    or (out, input_one_hot[0], input_one_hot[1], input_one_hot[2], input_one_hot[3], input_one_hot[4], input_one_hot[5], input_one_hot[6], input_one_hot[7], input_one_hot[8], input_one_hot[9], input_one_hot[10], input_one_hot[11], input_one_hot[12], input_one_hot[13], input_one_hot[14], input_one_hot[15]);
+    generate
+        for (i=0; i<2**N; i=i+1) begin
+            or(out, input_one_hot[i]);
+        end
+    endgenerate
 endmodule
