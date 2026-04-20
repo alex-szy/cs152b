07dd088e510b0b1e13c22a6db0a5177753a28e69
Teong Seng Tan <tanteongseng2@gmail.com>
2026-04-20 11:16:29 -0700

    Init lab2

---
+++ lab2/lab2.srcs/sources_1/new/rx.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/20/2026 10:52:00 AM
+// Design Name: 
+// Module Name: rx
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rx(
+    input wire clk,
+    input wire rst,
+    input wire rx_line,
+    output reg [7:0] data
+    );
+    
+    
+endmodule
---
+++ lab2/lab2.srcs/sources_1/new/tx.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/20/2026 11:14:53 AM
+// Design Name: 
+// Module Name: tx
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tx(
+    input wire clk,
+    input wire rst,
+    input wire send,
+    input wire [7:0] data,
+    output wire tx_line
+    );
+endmodule
