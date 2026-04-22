28731478904a9fc526356a5a42590411622db5aa
Teong Seng Tan <tanteongseng2@gmail.com>
2026-04-22 10:25:33 -0700

    Finished rx.v

--- lab2/lab2.srcs/sources_1/new/rx.v
+++ lab2/lab2.srcs/sources_1/new/rx.v
@@ -27,5 +27,30 @@ module rx(
     output reg [7:0] data
     );
     
+    reg [3:0] state;
+    reg [7:0] buf_reg;
+    
+    always @(posedge clk) begin
+        if (state == 0 && rx_line == 0) begin
+            state <= 4'b1;
+        end else if (state == 9) begin
+            if (rx_line == 1) begin
+                data <= buf_reg;
+            end
+            state <= 0;
+        end else begin
+            state <= state + 1;
+            case (state)
+                1: buf_reg[0] <= rx_line;
+                2: buf_reg[1] <= rx_line;
+                3: buf_reg[2] <= rx_line;
+                4: buf_reg[3] <= rx_line;
+                5: buf_reg[4] <= rx_line;
+                6: buf_reg[5] <= rx_line;
+                7: buf_reg[6] <= rx_line;
+                8: buf_reg[7] <= rx_line;
+            endcase
+         end
+    end
     
 endmodule
