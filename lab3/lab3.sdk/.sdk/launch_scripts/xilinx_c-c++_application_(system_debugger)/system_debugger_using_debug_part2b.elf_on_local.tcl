f39e17d7375f325760dff88fe0076236b5d21835
Alex Su <suziyue@gmail.com>
2026-05-13 10:50:46 -0700

    Finished lab3

---
+++ lab3/lab3.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_part2b.elf_on_local.tcl
@@ -0,0 +1,8 @@
+connect -url tcp:127.0.0.1:3121
+configparams mdm-detect-bscan-mask 2
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183B31ACDA"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183B31ACDA"} -index 0
+dow C:/Users/Student/Desktop/cs152b/lab3/lab3.sdk/part2b/Debug/part2b.elf
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183B31ACDA"} -index 0
+con
